83e89b9d7635d8720290a460fc4761d8be10e3d0
GalinaGao123 <2901634865@qq.com>
2017-07-10 22:01:43 +0800

    base line

---
+++ ydzx.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+'use strict'
+
+const program = require('commander')
+const service = require('./lib/service')
+const register = require('./lib/register')
+const comment = require('./lib/comment')
+
+async function run(func, ...args) {
+  await service.init()
+  console.log('\n### 棱镜核心已启动 ###\n\n')
+
+  await func(...args)
+
+  await service.shutdown()
+}
+
+program
+  .version('1.0.0')
+  .description('一点资讯营销工具')
+
+program
+  .command('register [n]')
+  .description('注册用户')
+  .alias('r')
+  .action(n => run(register, parseInt(n) || 10))
+
+program
+  .command('comment')
+  .description('评论文章')
+  .alias('c')
+  .action(n => run(comment))
+
+program.parse(process.argv)
