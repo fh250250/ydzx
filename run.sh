01e244c0dd3ffa322a5ca9273806e1f0b3c37a4a
GalinaGao123 <2901634865@qq.com>
2017-07-16 17:01:17 +0800

    fix

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ basepath=$(cd `dirname $0`; pwd)
 mkdir -p $basepath/log
 log_filename=$basepath/log/`date "+%Y_%m_%d_%H_%M_%S"`.log
 
-node index.js > $log_filename
+node $basepath/index.js > $log_filename
