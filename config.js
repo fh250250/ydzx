9021619fa66e2819ad6c279937746c1250a5c316
GalinaGao123 <2901634865@qq.com>
2017-07-16 15:12:28 +0800

    fix

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  proxy_platform: {
+    orderid: '939992315979163'
+  },
+  sms_platform: {
+    key: 'api-ygnzq99b',
+    password: 'qq1234567.',
+    sid: '44694'
+  },
+  channels: [
+    { id: 'u11272', name: '搞笑GIF' },
+    { id: 't1121', name: '街拍' },
+    { id: 's10671', name: '搞笑' },
+    { id: 'u11392', name: '趣图' },
+    // { id: 'u12400', name: '内涵图' },
+    // { id: 'u659', name: '摄影' },
+    // { id: 'u676', name: '女人' },
+    // { id: 't14342', name: '知性' },
+    // { id: 'u11088', name: '屌爆阅读' },
+  ]
+}
--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const utils = require('./lib/utils')
 const register = require('./lib/register')
 const comment = require('./lib/comment')
 const like = require('./lib/like')
+const service = require('./lib/service')
+
+function need_register_user() {
+  const coll = service.db.collection('users')
+
+  return coll.count({ type: 1, use_count: 0 }).then(count => count < 10)
+}
 
 utils.run(async () => {
-  await register(10)
+  if (await need_register_user()) { await register(10) }
+
   await comment(0, 20)
+
   await like()
 })
